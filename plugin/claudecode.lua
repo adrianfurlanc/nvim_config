e6766f6ec9b1c6b97ce12519ddb92ed508108f33
Adrian Furlan <adrian.furlan.c@gmail.com>
2026-07-16 12:48:23 +0200

    Silence Claude Server INFO notification

--- plugin/claudecode.lua
+++ plugin/claudecode.lua
@@ -1,5 +1,6 @@
 -- claudecode.nvim
 require("claudecode").setup({
+	log_level = "warn", -- INFO messages echo to the shell on exit
 	terminal = {
 		provider = "none", -- we will use tmux splits instead of Neovim terminal UI
 	},
